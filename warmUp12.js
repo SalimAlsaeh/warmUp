0b50c57d9f9f4b954ecd0dce2ded28a37f733864
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2019-10-31 09:53:45 +0100

    warmup12 done

--- warmUp12.js
+++ warmUp12.js
@@ -6,7 +6,9 @@ Input
         An integer value representing the size of the range
 
 Output
-    Your function should return the starting (minimum) and ending (maximum) numbers of the range in the form of an array/list comprised of two integers.
+    Your function should return the starting (minimum) and ending (maximum) numbers of the range in the form of 
+    an array/list comprised of two integers.
+
     Test Example
 
     let inputString = '1568141291110137';
@@ -22,3 +24,44 @@ Technical Details
     -The maximum size of a range will be 100 integers
     -The starting number of a range will be: 0 < n < 100
  */
+
+ function mysteryRange(inputString, range){
+    var arr = [];
+    var minimum;
+    var maximum;
+    
+    if (range >= 10) {
+        for (var i = 0; i < inputString.length; i++) {
+        if (typeof Number(inputString[i])  === "number") {
+            if (Number(inputString[i])  === 1 ) {
+                arr.push(Number(inputString[i] + inputString[i+1]));
+                i = i + 1;
+            } else {
+                arr.push(Number(inputString[i]));
+            }
+        }
+      }
+    } else{
+        for (var i = 0; i < inputString.length; i++) {
+            if (typeof Number(inputString[i])  === "number") {
+                 arr.push(Number(inputString[i]));
+            }
+        }
+    }
+
+    var rangeArr = [];
+    minimum = arr[0];
+    maximum = arr[0];
+    for (var i = 0; i < arr.length; i++) {
+        if (minimum > arr[i]) {
+            minimum = arr[i];
+        }
+        if (maximum < arr[i]) {
+            maximum = arr[i];
+        }
+    }
+    console.log(minimum, maximum)
+    rangeArr.push(minimum, maximum);
+
+    return rangeArr;
+ }
