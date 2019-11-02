67cd9a6619e13cedb2b65bea63e764d68dbc1eb3
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2019-11-02 09:54:32 +0100

    completed warmUp14

--- warmUp14.js
+++ warmUp14.js
@@ -1,7 +1,8 @@
 /*
 Say you have an array for which the i element is the price of a given stock on day i.
 
-If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+If you were only permitted to complete at most one transaction 
+//(i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
 
 Note that you cannot sell a stock before you buy one.
 
@@ -19,5 +20,36 @@ Explanation: In this case, no transaction is done, i.e. max profit = 0.
 */
 
 var maxProfit = function(prices) {
-  // YOUR CODE HERE
+	var buyingPrice = prices[0];
+	var maxProfit = 0;
+	// this loop is to identify the minimum buying price;
+	for (var i = 0; i < prices.length; i++) {
+		if (buyingPrice > prices[i]) {
+			buyingPrice = prices[i];
+		}
+		
+	}
+	// this loop is to identify the maximum selling price starting from the index after the minimum buying price;
+	var sellingPrice = prices[prices.indexOf(buyingPrice) + 1];
+	
+	for (var i = prices.indexOf(buyingPrice) + 1; i < prices.length; i++) {
+		if (sellingPrice < prices[i]) {
+			sellingPrice = prices[i];
+		}
+	}
+	// if the either the selling price or the buying price is undefined meaning that theere is no selling price or the array is not numbers,
+	// return 0
+
+	if (sellingPrice === undefined || buyingPrice === undefined) {
+		return maxProfit; // returns 0
+	}
+	// calcaulates the max profit
+	maxProfit = sellingPrice - buyingPrice;
+
+	// if the maxProfit is negative that means you are actually losing not profiting if you bought the stock, if not; returns it;
+	if (maxProfit <= 0) {
+		return 0;
+	}
+
+	return maxProfit;
 };
