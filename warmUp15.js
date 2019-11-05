a23c8c89b05ab19076fdafa4de3cf7d0df9c3167
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2019-11-05 09:45:39 +0100

    w15 done

--- warmUp15.js
+++ warmUp15.js
@@ -4,3 +4,35 @@
 //
 //
 // Note : you can`t use any built-in functions .
+
+function reverseStr(string) {
+	var reversedArr = [];
+	var count = 0;
+	var reversedWord = "";
+	var spacesCount = 0;
+
+	for (var i = 0; i < (string.length); i++) {
+		
+		if (string[i] === ' ' ) {
+			reversedArr[count] = reversedWord;
+			reversedWord = '';
+			count++;
+		}
+		if ( i === string.length - 1) {
+			reversedWord += string[i];
+			reversedArr[count] = reversedWord;
+			break;			
+		}
+		reversedWord += string[i];
+	}
+
+	var reversedStr = "";
+	for (var i = reversedArr.length - 1; i >= 0; i--) {
+		if (i === 1) {
+			reversedStr += reversedArr[i] + ' ';
+		} else {
+			reversedStr += reversedArr[i];
+		}
+	}
+	return reversedStr;
+}
