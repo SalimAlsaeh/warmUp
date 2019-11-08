b11f9e641ca89964c98ac548df9ebfa7112c21cc
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2019-11-08 10:00:27 +0100

    completed

--- warmUp17.js
+++ warmUp17.js
@@ -1,6 +1,8 @@
 // There is a special keyboard with all keys in a single row.
 // ​
-// Given a string keyboard of length 26 indicating the layout of the keyboard (indexed from 0 to 25), initially your finger is at index 0. To type a character, you have to move your finger to the index of the desired character. The time taken to move your finger from index i to index j is |i - j|.
+// Given a string keyboard of length 26 indicating the layout of the keyboard (indexed from 0 to 25), initially your finger is at index 0.
+// To type a character, you have to move your finger to the index of the desired character. 
+//The time taken to move your finger from index i to index j is |i - j|.
 // ​
 // You want to type a string word. Write a function to calculate how much time it takes to type it with one finger.
 // ​
@@ -27,6 +29,33 @@
 
 // ​
 // ​
-// var calculateTime = function(keyboard, word) {
-    
-// };
+var calculateTime = function(keyboard, word) {
+	var charIndexesArray = [];
+	var keyboardArray = keyboard.split('');
+	var wordArray = word.split('');
+	wordArray.forEach(function(element, index){
+		charIndexesArray.push(keyboardArray.indexOf(element));
+	});
+	var time = charIndexesArray[0]; // adding the time of travel of the first letter
+	// adding the time of travel between the chars
+	for (var i = 0; i < charIndexesArray.length - 1; i++) {
+		for (var j = i + 1; j < i + 2; j++) {
+			time += positify(charIndexesArray[i] - charIndexesArray[j]);
+		}
+	}
+
+	return time;
+};
+
+// a fucntion to return a poitive number between the addition of indexes
+function positify(num) {
+	if(num < 0) {
+		return (-1 * num);
+	}
+	return num;
+	
+}
+
+// 22 +  15 + 15 + 4 + 13 + 25 + 14 + 15
+// i=22  , i = 15, i = 15,          i = 4 ,       i= 13,  i = 25  i = 14  , i = 15
+// 22 + (22-15) + (15 - 15 ? 0 = 15)  + (15-4) + (4-13), (13 - 25),  (25 - 14) , (14 - 15) 
