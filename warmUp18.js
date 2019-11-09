f25633cf425dab3e7f691feeb5c3e1df826730f4
SalimAlsaeh <salem.ali.alsaeh@gmail.com>
2019-11-09 09:50:15 +0100

    i can't belive it works

--- warmUp18.js
+++ warmUp18.js
@@ -1,6 +1,7 @@
 // You are given an input string.
 
-// For each symbol in the string if it's the first character occurence, replace it with a '1', else replace it with the amount of times you've already seen it...
+// For each symbol in the string if it's the first character occurence,
+// replace it with a '1', else replace it with the amount of times you've already seen it...
 
 // But will your code be performant enough?
 // Examples:
@@ -10,3 +11,23 @@
 
 // input   =  "aaaaaaaaaaaa"
 // result  =  "123456789101112"
+
+function symbolReplacer(string) {
+	var stringArray = string.split('');
+	var resultArray = [];
+	var countsArray = [];
+	var str = "";
+	stringArray.forEach(function(element, index){
+		if (resultArray.includes(element) === false) {
+			var i = 1;
+			resultArray.push(element);
+			countsArray.push(i)
+			str += countsArray[resultArray.indexOf(element)];
+		} else {
+			var index = resultArray.indexOf(element);
+			countsArray[index] += 1;
+			str += countsArray[index];
+		}
+	});
+	return str;
+}
